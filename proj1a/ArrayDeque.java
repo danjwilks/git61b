ef847d5b41bf1d5a47f6cca900bbc3493ce5e56c
Daniel <s1851664@ed.ac.uk>
2019-06-07 16:55:36 +0100

    Created basic ArrayDeque instance variable and constructor

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,6 +1,28 @@
-public class ArrayDeque{
+public class ArrayDeque<item>{
 
-    git
+
+    item[] items;
+    int size;
+    int nextFirst;
+    int nextLast;
+
+    public ArrayDeque(){
+
+        items = (item[]) new Object[8];
+        size = 0;
+        nextFirst = 0;
+        nextLast = 1;
+
+    }
+
+    public void addFirst(item i){
+
+
+
+
+    }
+
+
 
 
 
