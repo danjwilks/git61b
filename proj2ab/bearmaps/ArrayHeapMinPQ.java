6731fc6fe52044d0d944876258e6ca553a25ce0b
Daniel <s1851664@ed.ac.uk>
2019-07-01 16:19:35 +0100

    added constructor and private class Node to be used in heap

--- proj2ab/bearmaps/ArrayHeapMinPQ.java
+++ proj2ab/bearmaps/ArrayHeapMinPQ.java
@@ -1,15 +1,23 @@
 package bearmaps;
 
+import java.util.ArrayList;
+
 public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T>{
 
+    private ArrayList<T> heap;
+
+    public ArrayHeapMinPQ(){
+        heap = new ArrayList<>(null);
+    }
+
+
     /* Adds an item with the given priority value. Throws an
-     * IllegalArgumentExceptionb if item is already present.
+     * IllegalArgumentException if item is already present.
      * You may assume that item is never null. */
     @Override
     public void add(T item, double priority){
 
     }
-
 
     /* Returns true if the PQ contains the given item. */
     @Override
@@ -39,6 +47,49 @@ public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T>{
      * doesn't exist. */
     @Override
     public void changePriority(T item, double priority){
+
+    }
+
+    private class Node implements Comparable<Node>{
+
+        T item;
+        Double priority;
+
+        private Node(T item, Double priority){
+            this.item = item;
+            this.priority = priority;
+        }
+
+        public T getItem(){
+            return this.item;
+        }
+
+        public Double getPriority(){
+            return this.priority;
+        }
+
+        @Override
+        public int compareTo(Node other) {
+            if (other == null) {
+                return -1;
+            }
+            return Double.compare(this.getPriority(), other.getPriority());
+        }
+
+        @Override
+        @SuppressWarnings("unchecked")
+        public boolean equals(Object o) {
+            if (o == null || o.getClass() != this.getClass()) {
+                return false;
+            } else {
+                return ((Node) o).getItem().equals(getItem());
+            }
+        }
+
+        @Override
+        public int hashCode() {
+            return item.hashCode();
+        }
 
     }
 
