8db570c59b58378885f14e6de9829f7e54eb9e07
Daniel <s1851664@ed.ac.uk>
2019-07-03 21:34:12 +0100

    created MyTrieSet with methods from interface, no methods work

---
+++ lab9/MyTrieSet.java
@@ -0,0 +1,38 @@
+import java.util.List;
+
+public class MyTrieSet implements TrieSet61B{
+
+    /** Clears all items out of Trie */
+    @Override
+    public void clear(){
+
+    }
+
+    /** Returns true if the Trie contains KEY, false otherwise */
+    @Override
+    public boolean contains(String key){
+        return true;
+    }
+
+    /** Inserts string KEY into Trie */
+    @Override
+    public void add(String key){
+
+    }
+
+    /** Returns a list of all words that start with PREFIX */
+    @Override
+    public List<String> keysWithPrefix(String prefix){
+        return null;
+    }
+
+    /** Returns the longest prefix of KEY that exists in the Trie
+     * Not required for Lab 9. If you don't implement this, throw an
+     * UnsupportedOperationException.
+     */
+    @Override
+    public String longestPrefixOf(String key){
+        return null;
+    }
+
+}
