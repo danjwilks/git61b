752e9b14e25a259e3367a1d7a10353d00353cc7d
Daniel <s1851664@ed.ac.uk>
2019-07-09 13:07:18 +0100

    created speed test for naive and kdtree

--- proj2ab/bearmaps/KDTreeTest.java
+++ proj2ab/bearmaps/KDTreeTest.java
@@ -1,12 +1,54 @@
 package bearmaps;
 
+import edu.princeton.cs.algs4.Stopwatch;
 import org.junit.Test;
 
 import java.util.*;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class KDTreeTest {
+
+    @Test
+    public void speedTest(){
+        Random random = new Random(10);
+        List<Point> list = new LinkedList<>();
+
+        for (int i = 1; i<100000; i++){
+
+            Point p = new Point(random.nextDouble()*100000,random.nextDouble()*100000);
+            list.add(p);
+        }
+
+        KdTree test = new KdTree(list);
+
+        Stopwatch KDTreeSW = new Stopwatch();
+        for (int i = 1; i<10000; i++){
+
+            double x = random.nextDouble()*10000;
+            double y = random.nextDouble()*10000;
+
+            test.nearest(x,y);
+        }
+        double KDTreeTime = KDTreeSW.elapsedTime();
+
+
+        NaivePointSet correct = new NaivePointSet(list);
+        Stopwatch NaiveSW = new Stopwatch();
+        for (int i = 1; i<10000; i++){
+
+            double x = random.nextDouble()*10000;
+            double y = random.nextDouble()*10000;
+
+            correct.nearest(x,y);
+        }
+        double NaiveTime = NaiveSW.elapsedTime();
+
+        System.out.println("Naive Time: " + NaiveTime);
+        System.out.println("KDTree Time: " + KDTreeTime);
+        assertTrue(KDTreeTime/NaiveTime < 0.1);
+    }
 
     @Test
     public void nearrestTest(){
@@ -30,18 +72,6 @@ public class KDTreeTest {
 
             assertEquals(correct.nearest(x,y),test.nearest(x,y));
         }
-
-
-
-
-//        Point p1 = new Point(1.1, 2.2);
-//        Point p2 = new Point(3.3, 4.4);
-//        Point p3 = new Point(-2.9, 4.2);
-//
-//        NaivePointSet correct = new NaivePointSet(List.of(p1,p2,p3));
-//        KdTree test = new KdTree(List.of(p1,p2,p3));
-//
-//        assertEquals(correct.nearest(3.0,4.0), test.nearest(3.0,4.0));
     }
 
 
